b550bac07c10eb488bd721b6a42e0f815f96fce3
shabab <shababulhassan441@gmail.com>
2025-03-10 16:29:59 +0500

    remove unneccessary files

--- app/(OLD)/match/[id]/page.js
+++
@@ -1,9 +0,0 @@
-import Scoreboard from "@/components/Scoreboard";
-
-export default async function MatchPage({ params }) {
-  return (
-    <div className="container mx-auto p-5">
-      <Scoreboard matchId={(await params).id} />
-    </div>
-  );
-}
